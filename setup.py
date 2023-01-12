9d29b61aeed98b788b952350bbaef5a4b9e4788b
Andrew Wong <ctawong@gmail.com>
2023-01-11 19:55:34 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -102,8 +102,8 @@ _deps = [
     "regex!=2019.12.17",
     "requests",
     "tensorboard",
-    "torch>=1.4",
-    "torchvision",
+    "torch=1.13",
+    "torchvision=0.14",
     "transformers>=4.21.0",
 ]
 
